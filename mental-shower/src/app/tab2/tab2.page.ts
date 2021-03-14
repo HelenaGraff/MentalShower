121086d1085be5a8bbda2da12d4ca3713940fa1d
andr92h1 <andr92h1@edu.easj.dk>
2021-03-14 14:38:47 +0100

    InfoIcon

--- mental-shower/src/app/tab2/tab2.page.ts
+++ mental-shower/src/app/tab2/tab2.page.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
+
 export class Tab2Page {
 
-  constructor() {}
+  constructor( private alertCtrl: AlertController)
+  {}
+
+  async presentAlert() {
+    const alert = await this.alertCtrl.create ({
+    message: 'Air Speed parameter allows you to influence the ventilator’s intensity.'
+   });
+
+    await alert.present ();
+
+  }
 
 }
