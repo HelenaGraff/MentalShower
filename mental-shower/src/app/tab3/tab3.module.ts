07c7c962ec983b710ea942c5e145b14ae32f6fa9
Andrei Popa <andreip0pa44@gmail.com>
2021-03-15 15:05:09 +0100

    Group cards

--- mental-shower/src/app/tab3/tab3.module.ts
+++ mental-shower/src/app/tab3/tab3.module.ts
@@ -19,4 +19,10 @@ import { Tab3PageRoutingModule } from './tab3-routing.module';
   ],
   declarations: [Tab3Page]
 })
-export class Tab3PageModule {}
+export class Tab3PageModule {
+
+  onIconClick(){
+    
+  }
+
+}
--- mental-shower/src/app/tab3/tab3.page.ts
+++ mental-shower/src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +8,12 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
-
+  constructor(private alertCtrl: AlertController) {}
+  async onIconClick(){
+    const alert = await this.alertCtrl.create ({
+      message:"message"
+     });
+  
+      await alert.present ();
+  }
 }
