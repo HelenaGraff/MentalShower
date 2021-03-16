943d0f193d038f895af4f445239950c14bac5f6f
andr92h1 <andr92h1@edu.easj.dk>
2021-03-16 15:39:56 +0100

    Revert "WeatherService"
    This reverts commit 3a62e979bfa818f5db7b4be7d6870f86c20c01fe.

--- mental-shower/src/app/tab1/tab1.page.ts
+++ mental-shower/src/app/tab1/tab1.page.ts
@@ -1,18 +1,12 @@
 import { Component } from '@angular/core';
-import { WeatherService } from 'src/app/weather.service';
-
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-
-
 export class Tab1Page {
 
-  constructor( weatherService: WeatherService) {}
+  constructor() {}
 
 }
-
-
