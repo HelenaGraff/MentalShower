3a62e979bfa818f5db7b4be7d6870f86c20c01fe
andr92h1 <andr92h1@edu.easj.dk>
2021-03-16 14:31:55 +0100

    WeatherService

--- mental-shower/src/app/tab1/tab1.page.ts
+++ mental-shower/src/app/tab1/tab1.page.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { WeatherService } from 'src/app/weather.service';
+
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
+
+
 export class Tab1Page {
 
-  constructor() {}
+  constructor( weatherService: WeatherService) {}
 
 }
+
+
