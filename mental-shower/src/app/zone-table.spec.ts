f8a0af31e1b439f5b341587e9b716e6c43f96d42
Andrei Popa <andreip0pa44@gmail.com>
2021-03-22 14:34:17 +0100

    firebase zone getting

---
+++ mental-shower/src/app/zone-table.spec.ts
@@ -0,0 +1,7 @@
+import { ZoneTable } from './zone-table';
+
+describe('ZoneTable', () => {
+  it('should create an instance', () => {
+    expect(new ZoneTable()).toBeTruthy();
+  });
+});
---
+++ mental-shower/src/app/zone-table.ts
@@ -0,0 +1,7 @@
+export class ZoneTable {
+    ClassroomID:string;
+    Seat1:string;
+    Seat2:string;
+    Seat3:string;
+    Seat4:string;
+}
