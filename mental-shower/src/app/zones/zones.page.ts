671123fa5f3caa253bdc44fa9e9ac8a8bf905d19
andr92h1 <andr92h1@edu.easj.dk>
2021-03-22 02:15:56 +0100

    close modal

--- mental-shower/src/app/zones/zones.page.ts
+++ mental-shower/src/app/zones/zones.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-zones',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ZonesPage implements OnInit {
 
-  constructor() { }
+  constructor(public modalController: ModalController) { }
 
   ngOnInit() {
   }
 
+
+async closeTab() {
+
+  console.log('Close');
+
+  
+    const modal = await this.modalController.dismiss();
+}
 }
