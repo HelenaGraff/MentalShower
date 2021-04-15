6d725a5d36c738bddf014f89ffaba2222a5d9a5a
Andrei Popa <andreip0pa44@gmail.com>
2021-04-15 13:39:36 +0200

    Preparing implementation

--- moodApi/server.js
+++ moodApi/server.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 db.sequelize.sync();
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome to mood API." });
 });
 
 // set port, listen for requests
